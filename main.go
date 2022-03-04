e0d1b73c0bc5d181ac1cbfb4897c04a4d8b8d8de
Salih GÜNEL <tonystark8861@gmail.com>
2022-03-04 21:09:33 +0300

    learned function

--- main.go
+++ main.go
@@ -7,19 +7,35 @@ package main // bu hangi pakete dahil olduğunu belirtemek için (javada da böy
 import (
 	//"gplesson/arrays"
 	//"gplesson/loops"
-	"gplesson/slices"
+	//"gplesson/slices"
+	//"fmt"
+	//"fmt"
+	"fmt"
+	"gplesson/functions"
 )
-
 
 func main() {
 
 	// variables.Variable()
 	// conditionals.Workshop()
 	//loops.Loop()
-    //loops.Workshop()
+	//loops.Workshop()
 	//arrays.Array()
 	//arrays.Workshop()
 	//arrays.Matrixarray()
-	slices.Slice2()
+	//slices.Slice2()
+	//var Sonuç = functions.Topla(2,6)
+	//fmt.Println(Sonuç)
+	//functions.SelamVer("Umut")
+	//sonuc1, sonuc2, sonuc3, _ := functions.DortIslem(10,6)
+	//fmt.Println("Toplam", sonuc1)
+	//fmt.Println("Cıkarma", sonuc2)
+	//fmt.Println("Carpma", sonuc3)
+	//fmt.Println("Bolme", sonuc4)
+	fmt.Println(functions.ToplaVariadic(1, 4, 6, 3, 10))
+	fmt.Println(functions.ToplaVariadic(1, 4))
+	fmt.Println(functions.ToplaVariadic())
 
+	sayilar := []int{4, 6, 7, 0, 11}
+	fmt.Println(functions.ToplaVariadic(sayilar...))
 }
